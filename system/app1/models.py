8a0a3d780ee64d8579cebb73aafec80a452f0d47
gemreyranola <gemreyranola@gmail.com>
2023-07-31 05:47:11 +0000

    Initial Commit

---
+++ system/app1/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Owner(models.Model):
+    pass
+
+class Staff(models.Model):
+    pass
+
+class ScrapType(models.Model):
+    pass
+
+class ScrapItem(models.Model):
+    pass
+
+class DailyScrapEntry(models.Model):
+    pass
