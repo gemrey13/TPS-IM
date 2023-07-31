94fd0ed01ec2f3bd6b0285e2d5215d4094b67284
gemrey <gemreyranola@gmail.com>
2023-08-01 06:59:49 +0800

    Added Models

--- system/app1/admin.py
+++ system/app1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(ScrapType)
+admin.site.register(ScrapItem)
+admin.site.register(DailyScrapEntry)
+admin.site.register(ScrapEntryDetail)
+admin.site.register(Transaction)
+admin.site.register(TransactionDetail)
+admin.site.register(UserProfile)
--- system/app1/models.py
+++ system/app1/models.py
@@ -1,19 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Owner(models.Model):
-    pass
-
-class Staff(models.Model):
-    pass
 
 class ScrapType(models.Model):
-    pass
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class ScrapItem(models.Model):
-    pass
+    RFID = models.CharField(max_length=100, unique=True)
+    weight = models.DecimalField(max_digits=10, decimal_places=2)
+    scrap_type = models.ForeignKey(ScrapType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"RFID: {self.RFID}, Type: {self.scrap_type}, Weight: {self.weight} kg"
 
 class DailyScrapEntry(models.Model):
-    pass
+    date = models.DateField()
+    scraps = models.ManyToManyField(ScrapItem, through='ScrapEntryDetail')
+
+    def __str__(self):
+        return f"Date: {self.date}"
 
 class ScrapEntryDetail(models.Model):
-    pass
+    daily_scrap_entry = models.ForeignKey(DailyScrapEntry, on_delete=models.CASCADE)
+    scrap_item = models.ForeignKey(ScrapItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"Daily Scrap Entry: {self.daily_scrap_entry}, Scrap Item: {self.scrap_item}, Quantity: {self.quantity}"
+
+class Transaction(models.Model):
+    date = models.DateField()
+    scraps = models.ManyToManyField(ScrapItem, through='TransactionDetail')
+
+    def __str__(self):
+        return f"Transaction Date: {self.date}"
+
+class TransactionDetail(models.Model):
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+    scrap_item = models.ForeignKey(ScrapItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"Transaction: {self.transaction}, Scrap Item: {self.scrap_item}, Quantity: {self.quantity}"
+
+
+class UserProfile(models.Model):
+    USER_TYPES = (
+        ('owner', 'Owner'),
+        ('staff', 'Staff'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=10, choices=USER_TYPES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.user_type}"
