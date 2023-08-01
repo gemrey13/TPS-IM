d87c9847e73122b761c092ff55dcc8e4f67398f8
gemrey <gemreyranola@gmail.com>
2023-08-01 08:09:11 +0800

    Added Table

--- system/app1/urls.py
+++ system/app1/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('daily_scrap_table/', views.daily_scrap_table, name='daily_scrap_table'),
+    path('add_daily_scrap_entry/', views.add_daily_scrap_entry, name='add_daily_scrap_entry'),
+    path('remove_daily_scrap_entry/<int:entry_id>/', views.remove_daily_scrap_entry, name='remove_daily_scrap_entry'),
 ]
--- system/app1/views.py
+++ system/app1/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import DailyScrapEntry, ScrapItem, ScrapEntryDetail
 
-# Create your views here.
+def daily_scrap_table(request):
+    daily_scrap_entries = DailyScrapEntry.objects.all()
+    return render(request, 'daily_scrap_table.html', {'daily_scrap_entries': daily_scrap_entries})
+
+def add_daily_scrap_entry(request):
+    if request.method == 'POST':
+        date = request.POST.get('date')
+        daily_scrap_entry = DailyScrapEntry.objects.create(date=date)
+
+        scrap_items = ScrapItem.objects.all()
+        for scrap_item in scrap_items:
+            quantity = request.POST.get('quantity_{}'.format(scrap_item.id))
+            if quantity and int(quantity) > 0:
+                ScrapEntryDetail.objects.create(daily_scrap_entry=daily_scrap_entry, scrap_item=scrap_item, quantity=quantity)
+
+        return redirect('daily_scrap_table')
+    else:
+        scrap_items = ScrapItem.objects.all()
+        return render(request, 'add_daily_scrap_entry.html', {'scrap_items': scrap_items})
+
+def remove_daily_scrap_entry(request, entry_id):
+    entry = DailyScrapEntry.objects.get(pk=entry_id)
+    entry.delete()
+    return redirect('daily_scrap_table')
